de1c04b1543421a25381b0fcbb96184597c630ef
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-08-21 15:57:52 +0530

    Update Toast.jsx

--- src/Components/Toast.jsx
+++ src/Components/Toast.jsx
@@ -6,15 +6,14 @@ const Toast = ({toast}) => {
 <>
 
   <div
-    className="flex items-center w-full max-w-xs p-4 mb-4 
-     bg-green-600 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+    className="flex items-center w-full max-w-xs p-4 mb-4  
+     bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
     role="alert"
   >
-    <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+    <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-black rounded-lg dark:bg-green-800 dark:text-green-200">
     </div>
     <div className="ml-3 text-sm font-normal">
-       {toast?<h1>Task Saved successfully</h1>:<h1>Something went wrong</h1>}
-      </div>
+      {toast?<h1 className='text-green-500'>Task Saved Successfully</h1>:<h1 className='text-red-500'>Something Went wrong</h1>}</div>
   </div>
   
   </>
