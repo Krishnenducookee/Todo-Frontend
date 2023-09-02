af9e9f53768e0c7da0bfaa568664e7994f72b2e3
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-09-02 15:45:08 +0530

    Update useFetch.jsx

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,18 +1,19 @@
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { MakeApiCall } from "./MakeApiCall";
 
 const useFetch=({
-    url,method,handleResponse,istyping
+    url,method,handleResponse
 })=>{
     const [fetchedData,setFetchedData]=useState([])
  
     useEffect(()=>{      
-        if(!istyping)
+        
    MakeApiCall({url,method}).then((response)=>{
         
-        setFetchedData(response.data.data )
-         handleResponse(response.data.data)
+        setFetchedData(response.data.data)
+        handleResponse(response.data.data)
+        
    }) 
     
     
