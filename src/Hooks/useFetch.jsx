ae686e880e8324e345435526909285bb0fba30dd
Krishnenducookee <141391502+Krishnenducookee@users.noreply.github.com>
2023-09-05 11:22:34 +0530

    Update useFetch.jsx

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -3,20 +3,26 @@ import { useEffect, useState } from "react";
 import { MakeApiCall } from "./MakeApiCall";
 
 const useFetch=({
-    url,method,handleResponse
+    url,method,handleResponse,editTaskId
 })=>{
     const [fetchedData,setFetchedData]=useState([])
- 
+     const urlsplittedarray=url.split('/')
     useEffect(()=>{      
-        
+     if(editTaskId)  { 
    MakeApiCall({url,method}).then((response)=>{
         
-        setFetchedData(response.data.data)
+         setFetchedData(response.data.data)
         handleResponse(response.data.data)
         
    }) 
-    
-    
+     }else if(!urlsplittedarray[1]){
+        MakeApiCall({url,method}).then((response)=>{
+        
+            setFetchedData(response.data.data)
+           handleResponse(response.data.data)
+           
+      }) 
+     }
      },[])
   
 }
